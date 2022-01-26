31d11cb15743e888b2aa34c025f781cb69b78248
Giuliano Beltrami <juka2027@live.com>
2022-01-26 10:33:22 -0300

    Navbar first

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Loader = () => {
+  return <div><h1>Loader</h1></div>;
+};
+
+export default Loader;
---
+++ client/src/components/Transactions.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Transactions = () => {
+  return <div><h1>Transactions</h1></div>;
+};
+
+export default Transactions;
