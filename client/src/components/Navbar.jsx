ac9e8384d26b76e4b174da84b42c8b5aa4d5344a
Giuliano Beltrami <juka2027@live.com>
2022-01-28 12:53:33 -0300

    End of Navbar, welcome and loader

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -43,9 +43,10 @@ const Navbar = () => {
           : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
         }
         {toggleMenu && (
-          <ul>
+          <ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none animate-slide-in
+            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white">
             <li className="text-xl w-full my-2">
-              <AiOutlineClose onclick={() => setToggleMenu(false)} />
+              <AiOutlineClose onClick={() => setToggleMenu(false)} />
             </li>
             <RenderNavbarItem classProps="my-2 text-lg"/>
           </ul>
