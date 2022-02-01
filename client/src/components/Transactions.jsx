2aff132d3b79e43954f0c94fab15271e466f5f87
Giuliano Beltrami <juka2027@live.com>
2022-02-01 16:49:19 -0300

    Inícion de transactions

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useContext } from "react";
+
+import { TransactionContext } from "../context/TransactionContext";
+
+import dummyData from "../utils/dummyData";
+import { shortenAddress } from '../utils/shortenAddress';
+
+const TransactionCard = ({addressTo, addressFrom,timestamp,message,keyword,amount,url}) => {
+  return (
+    <div className="bg-[#181918] m-4 flex flex-1 flex-col p-3 rounded-md hover:shadow-2xl
+      2xl:min-w-[450px]
+      2xl:max-w-[500px]
+      sm:min-w-[270px]
+      sm:max-w-[300px]      
+    ">
+      <div className="flex flex-col items-center w-full mt-3">
+
+      </div>
+    </div>
+  );
+}
 
 const Transactions = () => {
-  return <div><h1>Transactions</h1></div>;
+  const { currentAccount } = useContext(TransactionContext);
+
+  return (
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+      <div className="flex flex-col md:p-12 py-12 px-4">
+        {currentAccount
+          ? (
+            <h3 className="text-white text-3xl text-center my-2">
+              Latest transactions
+            </h3>
+            )
+          : (
+            <h3 className="text-white text-3xl text-center my-2">
+              Connect your account to see the latest transactions
+            </h3>
+            )
+        }
+        <div className="flex flex-wrap justify-center items-center mt-10">
+          {dummyData.reverse().map((transaction,index) => (
+            <TransactionCard key={index} {...transaction}/>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Transactions;
