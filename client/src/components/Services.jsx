887cd3aed01e2a8961d968d009a04810ac16de23
Giuliano Beltrami <juka2027@live.com>
2022-02-01 14:35:19 -0300

    Finalização dos contratos e início da services

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React from "react";
+import { BsShieldFillCheck } from "react-icons/bs";
+import { BiSearchAlt } from "react-icons/bi";
+import { RiHeart2Fill } from "react-icons/ri";
 
 const Services = () => {
   return <div><h1>Services</h1></div>;
